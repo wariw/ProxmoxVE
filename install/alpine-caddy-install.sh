b8e3f7a00b8be1b0a1d5069865c703a4fab62887
wariw <wariwpl@gmail.com>
2025-01-12 11:42:58 +0100

    Add alpine version of Caddy

---
+++ install/alpine-caddy-install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apk add newt
+$STD apk add curl
+$STD apk add openssh
+$STD apk add nano
+$STD apk add mc
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Alpine-Caddy"
+$STD apk add caddy
+$STD rc-update add caddy
+$STD rc-service caddy start
+msg_ok "Installed Alpine-Caddy"
+
+motd_ssh
+customize
